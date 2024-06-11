d12e077608f723e9698d55d577cbb354d3841879
zDragonSK <zdragonsk@pm.me>
2024-06-11 13:44:43 -0300

    Update readme

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-insertion-on-trigger nil nil nil "Customized with use-package company")
+ '(package-selected-packages
+   '(orderless flycheck-eglot flycheck-popup-tip flycheck-posframe flycheck vterm projectile editorconfig pdf-tools ob-async ob-rust marginalia vertico company-box company which-key doom-modeline treemacs-evil evil-collection evil-visual-mark-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
